065afcbc6849b32470a8b806d9a743639ba6e096
Ramiro <ramirojavadeveloper@gmail.com>
2024-12-17 00:12:08 -0300

    tentativa de adição de evento hover

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { AfterContentInit, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -6,8 +7,19 @@ import { Component } from '@angular/core';
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
-export class MenuComponent {
+export class MenuComponent implements AfterContentInit{
   abriuMenuHamburguer:boolean = false;
+  @ViewChild('listdropdown', { static: false }) listDropDown: any;
+
+  ngAfterContentInit():void{
+    if(this.listDropDown != undefined || this.listDropDown != null){
+      for(let link of this.listDropDown.nativeElement.children){
+        link.addEventListener("mouseover", () => { 
+          (link as HTMLElement).style.backgroundColor = "#318a8a"
+        });
+      }
+    }
+  }
 
   public manageMenu():void{
     this.abriuMenuHamburguer = !this.abriuMenuHamburguer;
