1417dfdabf6522a966231b5dc53ad0be4888a3cf
Ramiro <ramirojavadeveloper@gmail.com>
2024-12-17 08:25:11 -0300

    mudança comportamento da tela

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,5 @@
 import { DOCUMENT } from '@angular/common';
-import { AfterContentInit, Component, ElementRef, ViewChild} from '@angular/core';
+import { Component, inject} from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -7,22 +7,18 @@ import { AfterContentInit, Component, ElementRef, ViewChild} from '@angular/core
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
-export class MenuComponent implements AfterContentInit{
+export class MenuComponent {
   abriuMenuHamburguer:boolean = false;
-  @ViewChild('listdropdown', { static: false }) listDropDown: any;
+  document = inject(DOCUMENT);
 
-  ngAfterContentInit():void{
-    if(this.listDropDown != undefined || this.listDropDown != null){
-      for(let link of this.listDropDown.nativeElement.children){
-        link.addEventListener("mouseover", () => { 
-          (link as HTMLElement).style.backgroundColor = "#318a8a"
-        });
-      }
-    }
-  }
 
   public manageMenu():void{
     this.abriuMenuHamburguer = !this.abriuMenuHamburguer;
+    let listDropDown = this.document.getElementById("listdropdown");
+    if(this.abriuMenuHamburguer){
+      (listDropDown as HTMLElement).style.display = "block";
+      return;
+    }
+    (listDropDown as HTMLElement).style.display = "none";
   }
-
 }
