4c6485485bc90f5c70afdb88d19f475fcea0aa7b
Ramiro <ramirojavadeveloper@gmail.com>
2024-12-17 20:45:26 -0300

    adição de integração com whatsapp

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-content',
@@ -6,6 +6,18 @@ import { Component } from '@angular/core';
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit{
+  urlWhatsApp:string;
+  textoInicioConversa:string = "Olá, gostaria de fazer um orçamento para meu site."
+  numeroCelularEscolhido:string;
+
+  ngOnInit(): void {
+    let numeroAleatorio:number = (Math.random() >= 0.5) ? 1 : 0;
+
+    numeroAleatorio == 0 ? this.numeroCelularEscolhido = "5562993172731" : this.numeroCelularEscolhido = "5562993357679";
+
+    this.urlWhatsApp = "https://api.whatsapp.com/send?phone=" + this.numeroCelularEscolhido + "&text=" + this.textoInicioConversa;
+  }
+
 
 }
