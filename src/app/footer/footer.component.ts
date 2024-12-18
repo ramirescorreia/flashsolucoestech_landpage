c20a43fe6cd152051f20f476aa35ece0716b85a7
Ramiro <ramirojavadeveloper@gmail.com>
2024-12-18 08:15:09 -0300

    correção de css

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faFacebookF, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
@@ -9,9 +9,20 @@ import { faFacebookF, faInstagram, faLinkedin, faTwitter } from '@fortawesome/fr
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
   faFacebookF = faFacebookF;
   faInstagram = faInstagram;
   faLinkedin = faLinkedin;
   faTwitter = faTwitter;
+
+  urlWhatsAppRamiro:string;
+  urlWhatsAppAndre:string;
+  textoInicioConversa:string = "Olá, gostaria de fazer um orçamento para meu site."
+  celularRamiro:string = "5562993172731";
+  celularAndre:string = "5562993357679";
+
+  ngOnInit(): void {
+    this.urlWhatsAppRamiro = "https://api.whatsapp.com/send?phone=" + this.celularRamiro + "&text=" + this.textoInicioConversa;
+    this.urlWhatsAppAndre = "https://api.whatsapp.com/send?phone=" + this.celularAndre + "&text=" + this.textoInicioConversa;
+  }
 }
