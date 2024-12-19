168bc713a2d1bb73e155abcf809511d499105a9b
andregit <barbosaandre337@gmail.com>
2024-12-19 16:17:07 -0300

    termos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { PoliticaPrivacidadeComponent } from './politica-privacidade/politica-privacidade.component';
 import { HomeComponent } from './home/home.component';
+import { TermosCondicoesComponent } from './termos-condicoes/termos-condicoes.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
+    {path: 'termos-e-condicoes' , component: TermosCondicoesComponent},
     {path: 'politica-de-privacidade', component: PoliticaPrivacidadeComponent},
     {path: '', redirectTo: 'home', pathMatch: 'full'}
     
--- src/app/termos-condicoes/termos-condicoes.component.ts
+++ src/app/termos-condicoes/termos-condicoes.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-termos-condicoes',
   standalone: true,
+  imports: [FooterComponent],
   templateUrl: './termos-condicoes.component.html',
   styleUrl: './termos-condicoes.component.css',
 })
